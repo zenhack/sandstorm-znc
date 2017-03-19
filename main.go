d8407cf6232905bce74bc9eecd28b5121aee59ff
Ian Denhardt <ian@zenhack.net>
2017-03-19 01:55:09 -0400

    Move writeConfig to main.go
    This really didn't deserve its own file.

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"context"
+	"os"
 	ip_capnp "zenhack.net/go/sandstorm/capnp/ip"
 	"zenhack.net/go/sandstorm/grain"
+	// We import this under a different name, since we use
+	// "html/template" as well. "text/template" is only used
+	// for znc.conf.
+	txtTpl "text/template"
 )
+
+var (
+	zncConfTpl = txtTpl.Must(txtTpl.ParseFiles("/opt/app/znc.conf"))
+)
+
+type ZncConfig struct {
+	ListenPort, DialPort string
+}
+
+func writeConfig(cfg *ZncConfig) {
+	configDir := os.Getenv("HOME") + "/.znc/configs"
+	chkfatal(os.MkdirAll(configDir, 0700))
+	file, err := os.Create(configDir + "/znc.conf")
+	chkfatal(err)
+	defer file.Close()
+	chkfatal(zncConfTpl.Execute(file, cfg))
+}
 
 func main() {
 	ctx := context.Background()
