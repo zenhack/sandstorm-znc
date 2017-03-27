a36dc343807ff83e7c423e5872b8730ece6424e2
Ian Denhardt <ian@zenhack.net>
2017-03-26 20:34:26 -0400

    Make it possible to save config modifications

--- main.go
+++ main.go
@@ -20,6 +20,17 @@ type ZncConfig struct {
 	ListenPort, DialPort string
 }
 
+// Check if a file exists.
+func exists(filename string) (bool, error) {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 // Write the znc config to the appropriate location.
 func writeConfig(cfg *ZncConfig) {
 	configDir := os.Getenv("HOME") + "/.znc/configs"
@@ -38,10 +49,31 @@ func main() {
 
 	go ipNetworkProxy(ctx, netCaps, configs)
 
-	writeConfig(&ZncConfig{
-		ListenPort: zncPort,
-		DialPort:   ipNetworkPort,
-	})
+	// We use the file ${HOME}/HAVE_CONFIG to indicate whether we've
+	// written the config in the past; it will only be absent on the first
+	// bringup. We don't want to override it if so, since the user may
+	// have asked ZNC to apply changes.
+	//
+	// We could just check for the presense of the config file itself,
+	// but that has two problems:
+	//
+	// 1. For users who installed alpha 1, we want this to upgrade the
+	//    (non-user-modifiable) config that that version created.
+	// 2. In the even that the system dies in the middle of writing the
+	//    config, checking for the config woud permanently corrupt the
+	//    grain, since we'd never remove/overwrite the partial config.
+	haveCfgPath := os.Getenv("HOME") + "/HAVE_CONFIG"
+	haveCfg, err := exists(haveCfgPath)
+	chkfatal(err)
+	if !haveCfg {
+		writeConfig(&ZncConfig{
+			ListenPort: zncPort,
+			DialPort:   ipNetworkPort,
+		})
+		file, err := os.Create(haveCfgPath)
+		chkfatal(err)
+		file.Close()
+	}
 
 	api, err := grain.ConnectAPI(ctx, webui(ctx, netCaps, configs))
 	chkfatal(err)
